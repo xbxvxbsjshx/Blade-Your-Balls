e759bac7acd519c9088d9bdb83d1405be374ac13
xbxvxbsjshx <155461387+xbxvxbsjshx@users.noreply.github.com>
2024-01-15 12:12:22 +0200

    Create Manual-Block.lua

---
+++ Manual-Block.lua
@@ -0,0 +1,22 @@
+local gui, frame, button = Instance.new("ScreenGui", game.CoreGui), Instance.new("Frame"), Instance.new("TextButton")
+gui.ResetOnSpawn = false
+frame.Size, frame.Position, frame.BackgroundColor3, frame.BorderSizePixel, frame.Active, frame.Draggable, frame.Parent = UDim2.new(0, 150, 0, 75), UDim2.new(0, 10, 0, 10), Color3.new(0, 0, 0), 0, true, true, gui
+button.Text, button.Size, button.Position, button.BackgroundColor3, button.BorderColor3, button.BorderSizePixel, button.Font, button.TextColor3, button.TextSize, button.Parent = "Manual Spam", UDim2.new(1, -20, 1, -20), UDim2.new(0, 10, 0, 10), Color3.new(0, 0, 0), Color3.new(), 2, Enum.Font.SourceSans, Color3.new(1, 1, 1), 16, frame
+
+local activated = false
+
+local function toggle()
+    activated, button.Text = not activated, activated and "🄾🄵🄵" or "🄾🄽"
+    
+    while activated do
+        local args = {1.5, CFrame.new(-254.29, 112.14, -119.27) * CFrame.Angles(-2.03, 0.57, 2.31), {["2617721424"] = Vector3.new(-273.40, -724.80, -20.92)}, {910, 154}}
+        game:GetService("ReplicatedStorage").Remotes.ParryAttempt:FireServer(unpack(args))
+        game:GetService("RunService").Heartbeat:Wait()
+        button.BorderColor3 = Color3.new(math.random(), math.random(), math.random())
+    end
+end
+
+button.MouseButton1Click:Connect(function()
+    toggle()
+    showNotification()
+end)
